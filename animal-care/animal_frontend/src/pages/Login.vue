304d7ba2e148ce500df4d6a7557bad4d0920cf6a
ramaadityaf <ramaadityaf29@gmail.com>
2025-10-01 13:32:11 +0700

    update loginp

--- animal-care/animal_frontend/src/pages/Login.vue
+++ animal-care/animal_frontend/src/pages/Login.vue
@@ -2,6 +2,20 @@
   <div class="container-fluid vh-100 d-flex align-items-center justify-content-center text-white"
      style="background-color: #5b6ef5;">
 
+    <nav
+      class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top"
+      style="background-color: #5b6ef5; z-index: 1000;">
+      <div class="container">
+        <a class="navbar-brand fw-bold text-white d-flex align-items-center"
+          href="#">
+          <img src="/animal.png" alt="logo" class="logo-navbar me-2" />
+          <span class="fs-4">
+            Animal <span class="text-warning">Care</span>
+          </span>
+        </a>
+      </div>
+    </nav>
+
     <div class="row w-100">
       <div class="col-md-6 d-flex flex-column justify-content-center align-items-start ps-5">
         <h2 class="fw-bold">Form Login</h2>
@@ -76,6 +90,12 @@ export default {
 .btn-custom:hover {
   background-color: #5b6ef5; /* warna saat hover */
   color: #fff;
+}
+
+/* Logo navbar biar seimbang */
+.logo-navbar {
+  height: 56px; 
+  width: auto;
 }
 
 </style>
--- animal-care/animal_frontend/src/pages/Regis.vue
+++ animal-care/animal_frontend/src/pages/Regis.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="container-fluid vh-100 d-flex align-items-center justify-content-center text-white"
-     style="background-color: #5b6ef5;">
+  <!-- padding agar form tidak ketutup -->
+  <div
+    class="container-fluid vh-100 d-flex align-items-center justify-content-center text-white"
+    style="background-color: #5b6ef5; padding-top: 80px;"
+  >
+    <nav
+      class="navbar navbar-expand-lg navbar-dark shadow-sm fixed-top"
+      style="background-color: #5b6ef5; z-index: 1000;"
+    >
+      <div class="container">
+        <a class="navbar-brand fw-bold text-white d-flex align-items-center" href="#">
+          <img src="/animal.png" alt="logo" class="logo-navbar me-2" />
+          <span class="fs-4">
+            Animal <span class="text-warning">Care</span>
+          </span>
+        </a>
+      </div>
+    </nav>
 
     <div class="row w-100">
       <div class="col-md-6 d-flex flex-column justify-content-center align-items-start ps-5">
@@ -31,15 +47,14 @@
               <input type="password" v-model="form.password" class="form-control" required />
             </div>
 
-          <button type="submit" class="btn btn-custom w-100 fw-bold">
-            Kirim
-          </button>
+            <button type="submit" class="btn btn-custom w-100 fw-bold">
+              Kirim
+            </button>
 
             <p class="mt-3 text-center">
               Sudah punya akun?
               <router-link to="/login" class="fw-bold text-decoration-none">Login</router-link>
             </p>
-
           </form>
         </div>
       </div>
@@ -70,10 +85,6 @@ export default {
 </script>
 
 <style scoped>
-.container-fluid {
-  background-color: #5b6ef5; 
-}
-
 .btn-custom {
   background-color: #485ae3ff;
   color: #fff;
@@ -81,7 +92,12 @@ export default {
 }
 
 .btn-custom:hover {
-  background-color: #5b6ef5; /* warna saat hover */
+  background-color: #5b6ef5;
   color: #fff;
+}
+
+.logo-navbar {
+  height: 45px;
+  width: auto;
 }
 </style>
